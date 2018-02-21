1e4bb336eeda04f4738eec3253216dcd272f2d1e
Luis Perez <lperez@belatrixsf.com>
2018-02-21 18:46:22 -0500

    ADDED macro and cell viewcontroller for signup process

---
+++ GoodShepherd/Controller/Authentication/Signup/TVCells/MacroLeaderTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  MacroLeaderTVCell.swift
+//  GoodShepherd
+//
+//  Created by Luis Arturo Perez Ramirez on 2/21/18.
+//  Copyright © 2018 Term. All rights reserved.
+//
+
+import UIKit
+
+class MacroLeaderTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
